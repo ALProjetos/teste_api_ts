8ad87b099afc93fadaff4da4cac11a3b54923366
Adam <adam.carlos86@gmail.com>
2024-05-27 22:50:21 -0300

    Ajuste

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3005;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, world!');
+  res.send('Hello, world! V2');
 });
 
 app.listen(port, () => {
